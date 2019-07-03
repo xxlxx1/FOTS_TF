58c8ac4bc8c45d2743e85d1e6e4b66ede8ed67fe
xiaxiaoling <xiaxiaoling@wezhuiyi.com>
2019-07-03 11:49:00 +0800

    增加打印信息

--- eval.py
+++ eval.py
@@ -5,22 +5,16 @@ import os
 import numpy as np
 import tensorflow as tf
 
-import locality_aware_nms as nms_locality
+
 import lanms
-import Levenshtein
 from bktree import BKTree, levenshtein, dict_words
 
-# tf.app.flags.DEFINE_string('test_data_path', 'training_samples/', '')
-# tf.app.flags.DEFINE_string('test_data_path', '/data2/data/15ICDAR/test/image/', '')
+
 tf.app.flags.DEFINE_string('test_data_path', '/home/qz/data/ICDAR15/ch4_test_images/', '')
-# tf.app.flags.DEFINE_string('test_data_path', '/home/qz/data/ICDAR15/ch4_training_images/', '')
 tf.app.flags.DEFINE_string('gpu_list', '1', '')
 tf.app.flags.DEFINE_string('checkpoint_path', 'checkpoints_pretrained_600k/', '')
-# tf.app.flags.DEFINE_string('checkpoint_path', 'checkpoints_15only/', '')
-# tf.app.flags.DEFINE_string('checkpoint_path', 'synth_pretrained_model/', '')
 tf.app.flags.DEFINE_string('output_dir', 'outputs/', '')
 tf.app.flags.DEFINE_bool('no_write_images', True, 'do not write images')
-# tf.app.flags.DEFINE_bool('use_vacab', True, 'strong, normal or weak')
 tf.app.flags.DEFINE_bool('use_vacab', True, 'strong, normal or weak')
 
 from module import Backbone_branch, Recognition_branch, RoI_rotate
@@ -47,25 +41,6 @@ def get_images():
                     break
     print('Find {} images'.format(len(files)))
     return files
-"""
-# It seems do not need it
-def resize_image(im, max_side_len=2400, input_size=512):
-    new_h, new_w, _ = im.shape
-    max_h_w_i = np.max([new_h, new_w, input_size])
-    im_padded = np.zeros((max_h_w_i, max_h_w_i, 3), dtype=np.uint8)
-    im_padded[:new_h, :new_w, :] = im.copy()
-    im = im_padded
-    # resize the image to input size
-    new_h, new_w, _ = im.shape
-    resize_h = input_size
-    resize_w = input_size
-    im = cv2.resize(im, dsize=(resize_w, resize_h))
-
-    resize_ratio_3_x = resize_w/float(new_w)
-    resize_ratio_3_y = resize_h/float(new_h)
-
-    return im, (resize_ratio_3_y, resize_ratio_3_x)
-"""
 
 def resize_image(im, max_side_len=2400):
     '''
@@ -124,7 +99,7 @@ def detect(score_map, geo_map, timer, score_map_thresh=0.8, box_thresh=0.1, nms_
     boxes = np.zeros((text_box_restored.shape[0], 9), dtype=np.float32)
     boxes[:, :8] = text_box_restored.reshape((-1, 8))
     boxes[:, 8] = score_map[xy_text[:, 0], xy_text[:, 1]]
-    timer['restore'] = time.time() - start
+    # timer['restore'] = time.time() - start
     # nms part
     start = time.time()
     # boxes = nms_locality.nms_locality(boxes.astype(np.float64), nms_thres)
@@ -140,52 +115,9 @@ def detect(score_map, geo_map, timer, score_map_thresh=0.8, box_thresh=0.1, nms_
         cv2.fillPoly(mask, box[:8].reshape((-1, 4, 2)).astype(np.int32) // 4, 1)
         boxes[i, 8] = cv2.mean(score_map, mask)[0]
     boxes = boxes[boxes[:, 8] > box_thresh]
-
+    print('{} text boxes after nms'.format(boxes.shape[0]))
     return boxes, timer
-"""
-def get_project_matrix_and_width(text_polyses, target_height=8.0):
-    project_matrixes = []
-    box_widths = []
-    filter_box_masks = []
-    # max_width = 0
-    max_width = 0
 
-    for i in range(text_polyses.shape[0]):
-        x1, y1, x2, y2, x3, y3, x4, y4 = text_polyses[i] / 4
-
-        rotated_rect = cv2.minAreaRect(np.array([[x1, y1], [x2, y2], [x3, y3], [x4, y4]]))
-        box_w, box_h = rotated_rect[1][0], rotated_rect[1][1]
-
-        if box_w <= box_h:
-            box_w, box_h = box_h, box_w
-
-        mapped_x1, mapped_y1 = (0, 0)
-        mapped_x4, mapped_y4 = (0, 8)
-
-        width_box = math.ceil(8 * box_w / box_h)
-        width_box = int(min(width_box, 128)) # not to exceed feature map's width
-        # width_box = int(min(width_box, 512)) # not to exceed feature map's width
-
-        if width_box > max_width: 
-            max_width = width_box 
-
-        mapped_x2, mapped_y2 = (width_box, 0)
-        mapped_x3, mapped_y3 = (width_box, 8)
-
-        src_pts = np.float32([(x1, y1), (x2, y2),(x3, y3), (x4, y4)])
-        dst_pts = np.float32([(mapped_x1, mapped_y1), (mapped_x2, mapped_y2), (mapped_x3, mapped_y3), (mapped_x4, mapped_y4)])
-
-        project_matrix = cv2.getPerspectiveTransform(dst_pts.astype(np.float32), src_pts.astype(np.float32))
-        project_matrix = project_matrix.flatten()[:8]
-
-        project_matrixes.append(project_matrix)
-        box_widths.append(width_box)
-
-    project_matrixes = np.array(project_matrixes)
-    box_widths = np.array(box_widths)
-
-    return project_matrixes, box_widths, max_width
-"""
 def get_project_matrix_and_width(text_polyses, target_height=8.0):
     project_matrixes = []
     box_widths = []
@@ -238,18 +170,7 @@ def sort_poly(p):
         return p
     else:
         return p[[0, 3, 2, 1]]
-"""
-def find_similar_word(input_str, word_set):
-    min_distance = 10000
-    best_word = input_str
-    for word in word_set:
-        dist = Levenshtein.distance(input_str, word)
-        if dist < min_distance:
-            min_distance = dist
-            best_word = word
 
-    return best_word
-"""
 def bktree_search(bktree, pred_word, dist=5):
     return bktree.query(pred_word, dist)
 
@@ -274,7 +195,7 @@ def main(argv=None):
         input_box_widths = tf.placeholder(tf.int32, shape=[None], name='input_box_widths')
         # input_box_nums = tf.placeholder(tf.int32, name='input_box_nums')
         # input_seq_len = tf.placeholder(tf.int32, shape=[None], name='input_seq_len')
-        input_seq_len = input_box_widths[tf.argmax(input_box_widths, 0)] * tf.ones_like(input_box_widths)
+        # input_seq_len = input_box_widths[tf.argmax(input_box_widths, 0)] * tf.ones_like(input_box_widths)
         global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
 
         shared_feature, f_score, f_geometry = detect_part.model(input_images)
@@ -298,18 +219,12 @@ def main(argv=None):
                 im_resized, (ratio_h, ratio_w) = resize_image(im, max_side_len=1024)
                 # im_resized_d, (ratio_h_d, ratio_w_d) = resize_image_detection(im)
 
-                timer = {'net': 0, 'restore': 0, 'nms': 0}
+                timer = {'rec_net':0, 'all_net': 0, 'get_det_socremap': 0, 'nms': 0}
                 start = time.time()
                 score, geometry = sess.run([f_score, f_geometry], feed_dict={input_images: [im_resized]})
-                
+                timer["get_det_socremap"] = time.time() - start
                 boxes, timer = detect(score_map=score, geo_map=geometry, timer=timer)
-                
-                """
-                if boxes is not None:
-                    boxes = boxes[:, :8].reshape((-1, 4, 2))
-                    boxes[:, :, 0] /= ratio_w
-                    boxes[:, :, 1] /= ratio_h
-                """
+
                 # save to file
                 if boxes is not None and boxes.shape[0] != 0:
                     res_file = os.path.join(
@@ -318,20 +233,16 @@ def main(argv=None):
                             os.path.basename(im_fn).split('.')[0]))
 
                     input_roi_boxes = boxes[:, :8].reshape(-1, 8)
-                    # input_roi_boxes = boxes[:, :8].reshape((-1, 4, 2))
-                    
-                    # input_roi_boxes = boxes.copy()
-                    # input_roi_boxes[:, :, 0] *= ratio_w
-                    # input_roi_boxes[:, :, 1] *= ratio_h
-                    # input_roi_boxes = input_roi_boxes.reshape((-1, 8))
-                    # boxes_masks = np.array([0] * input_roi_boxes.shape[0])
-                    boxes_masks = [0] * input_roi_boxes.shape[0]
-                    transform_matrixes, box_widths = get_project_matrix_and_width(input_roi_boxes)
-                    # max_box_widths = max_width * np.ones(boxes_masks.shape[0]) # seq_len
 
+                    boxes_masks = [0] * input_roi_boxes.shape[0]
+                    print("input_roi_boxes shape:", input_roi_boxes.shape)
+                    transform_matrixes, box_widths = get_project_matrix_and_width(input_roi_boxes)
+                    rec_start = time.time()
                     # Run end to end
-                    recog_decode = sess.run(dense_decode, feed_dict={input_images: [im_resized], input_transform_matrix: transform_matrixes, input_box_mask[0]: boxes_masks, input_box_widths: box_widths})
-                    timer['net'] = time.time() - start
+                    recog_decode = sess.run(dense_decode, feed_dict={input_images: [im_resized],
+                        input_transform_matrix: transform_matrixes, input_box_mask[0]: boxes_masks, input_box_widths: box_widths})
+                    timer['rec_net'] = time.time() - rec_start
+                    timer['all_net'] = time.time() - start
                     
                     # Preparing for draw boxes
                     boxes = boxes[:, :8].reshape((-1, 4, 2))
@@ -360,9 +271,6 @@ def main(argv=None):
                             f.write('{},{},{},{},{},{},{},{},{}\r\n'.format(
                                 box[0, 0], box[0, 1], box[1, 0], box[1, 1], box[2, 0], box[2, 1], box[3, 0], box[3, 1], recognition_result))
 
-                            # f.write('{},{},{},{},{},{},{},{}\r\n'.format(
-                            #     box[0, 0], box[0, 1], box[1, 0], box[1, 1], box[2, 0], box[2, 1], box[3, 0], box[3, 1]
-                            # ))
                             # Draw bounding box
                             cv2.polylines(im[:, :, ::-1], [box.astype(np.int32).reshape((-1, 1, 2))], True, color=(255, 255, 0), thickness=1)
                             # Draw recognition results area
@@ -374,14 +282,14 @@ def main(argv=None):
                             cv2.fillPoly(im[:, :, ::-1], [text_area.astype(np.int32).reshape((-1, 1, 2))], color=(255, 255, 0))
                             im_txt = cv2.putText(im[:, :, ::-1], recognition_result, (box[0, 0], box[0, 1]), font, 0.5, (0, 0, 255), 1)
                 else:
-                    timer['net'] = time.time() - start
+                    timer['all_net'] = time.time() - start
                     res_file = os.path.join(FLAGS.output_dir, 'res_' + '{}.txt'.format(os.path.basename(im_fn).split('.')[0]))
                     f = open(res_file, "w")
                     im_txt = None
                     f.close()
 
-                print('{} : net {:.0f}ms, restore {:.0f}ms, nms {:.0f}ms'.format(
-                    im_fn, timer['net']*1000, timer['restore']*1000, timer['nms']*1000))
+                print('{} : get_det_socremap {:.0f}ms, nms {:.0f}ms, rec_net {:.0f}ms, all_net {:.0f}ms'.format(
+                    im_fn, timer['get_det_socremap']*1000, timer['nms']*1000, timer['rec_net']*1000, timer['all_net']*1000))
 
                 duration = time.time() - start_time
                 print('[timing] {}'.format(duration))
